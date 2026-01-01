f58ef18f92c6175ba53c95e70f1b332c5c225620
warren kaunda <austinwarry@gmail.com>
2026-01-01 23:27:46 +0200

    ok

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,49 +1,49 @@
 const newsDisplay = document.querySelector(".news-container");
 
 function loadScrollReveal() {
-    let config = {
-        after: "0s",
-        enter: "bottom",
-        move: "24px",
-        over: ".66s",
-        easing: "ease-in-out",
-        viewportFactor: 0.33,
-        reset: false,
-        init: false,
-    };
+  let config = {
+    after: "0s",
+    enter: "bottom",
+    move: "24px",
+    over: ".66s",
+    easing: "ease-in-out",
+    viewportFactor: 0.33,
+    reset: false,
+    init: false,
+  };
 
-    window.scrollReveal = new scrollReveal(config);
-    scrollReveal.init();
+  window.scrollReveal = new scrollReveal(config);
+  scrollReveal.init();
 }
 
 function getNews() {
-    fetch("news.csv")
-        .then((response) => response.text())
-        .then((data) => {
-            let news = Papa.parse(data, { header: true }).data;
+  fetch("news.csv")
+    .then((response) => response.text())
+    .then((data) => {
+      let news = Papa.parse(data, { header: true }).data;
 
-            filterNews(news);
-        });
+      filterNews(news);
+    });
 }
 
 function filterNews(news) {
-    newsDisplay.innerHTML = "";
+  newsDisplay.innerHTML = "";
 
-    for (let i = news.length; i > 0; i--) {
-        let theNews = news[i - 1];
-        let tag = "old";
+  for (let i = news.length; i > 0; i--) {
+    let theNews = news[i - 1];
+    let tag = "old";
 
-        if (theNews.date == "new") {
-            tag = "new";
-        }
+    if (theNews.date == "new") {
+      tag = "new";
+    }
 
-        let newNews = document.createElement("div");
-        newNews.classList.add("news-item");
-        newNews.classList.add("shadow");
-        newNews.classList.add("text-center");
-        newNews.classList.add(tag);
-        newNews.setAttribute("data-scroll-reveal", "enter bottom move 24px");
-        newNews.innerHTML = `
+    let newNews = document.createElement("div");
+    newNews.classList.add("news-item");
+    newNews.classList.add("shadow");
+    newNews.classList.add("text-center");
+    newNews.classList.add(tag);
+    newNews.setAttribute("data-scroll-reveal", "enter bottom move 24px");
+    newNews.innerHTML = `
                             <a title="News Image" class="newsImage" href="./assets/img/picture/${theNews.name}.${theNews.format}" data-lightbox="news${theNews.index}"
                                 data-title="${theNews.headline}">
                                 <img width="100%" loading="lazy" src="./assets/img/picture/${theNews.name}.${theNews.format}" alt="News Image ${theNews.index}">
@@ -60,49 +60,51 @@ function filterNews(news) {
                             </div>
         `;
 
-        newsDisplay.appendChild(newNews);
-    }
+    newsDisplay.appendChild(newNews);
+  }
 
-    tagNew();
+  tagNew();
 }
 
 function tagNew() {
-    const newsItems = document.querySelectorAll(".news-item");
+  const newsItems = document.querySelectorAll(".news-item");
 
-    newsItems.forEach((newsItem) => {
-        if (newsItem.classList.contains("new")) {
-            let newTag = document.createElement("span");
-            newTag.classList.add("newTag");
-            newTag.innerHTML = `
+  newsItems.forEach((newsItem) => {
+    if (newsItem.classList.contains("new")) {
+      let newTag = document.createElement("span");
+      newTag.classList.add("newTag");
+      newTag.innerHTML = `
                 <p>New</p>
                 <i class="bi bi-fire"></i>
             `;
-            newsItem.querySelector(".newsImage").appendChild(newTag);
-        }
-    });
+      newsItem.querySelector(".newsImage").appendChild(newTag);
+    }
+  });
 }
 
 const getFormRTOHome = document.querySelector(".get-rto-form-home");
 const getCharterRTOHome = document.querySelector(".get-rto-charter-home");
 
 getFormRTOHome.onclick = () => {
-    const link = document.createElement("a");
-    link.setAttribute(
-        "href",
-        "./assets/data/pdf/Rent to Own - Application Form.pdf"
-    );
-    link.download = "Rent to Own - Application Form.pdf";
-    link.href = "./assets/data/pdf/Rent to Own - Application Form.pdf";
-    link.click();
+  const link = document.createElement("a");
+  link.setAttribute(
+    "href",
+    "./assets/data/pdf/Rent to Own - Application Form.pdf"
+  );
+  link.download = "Rent to Own - Application Form.pdf";
+  link.href = "./assets/data/pdf/Rent to Own - Application Form.pdf";
+  link.click();
 };
 
 getCharterRTOHome.onclick = () => {
-    const link = document.createElement("a");
-    link.setAttribute(
-        "href",
-        "./assets/data/pdf/Rent to Own - Service Charter.pdf"
-    );
-    link.download = "Rent to Own - Service Charter.pdf";
-    link.href = "./assets/data/pdf/Rent to Own - Service Charter.pdf";
-    link.click();
+  const link = document.createElement("a");
+  link.setAttribute(
+    "href",
+    "./assets/data/pdf/Rent to Own - Service Charter.pdf"
+  );
+  link.download = "Rent to Own - Service Charter.pdf";
+  link.href = "./assets/data/pdf/Rent to Own - Service Charter.pdf";
+  link.click();
 };
+
+// FOR IMPACT ANIMATION
